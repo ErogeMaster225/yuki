a364e867cdcd261e89da0125853c1ed7a8d0eb65
Trong Sang Nguyen <33416398+ErogeMaster225@users.noreply.github.com>
2025-01-09 01:17:06 +0700

    enable wireguard support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,6 +21,7 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  services.resolved.enable = true;
   nix.settings.experimental-features = "nix-command flakes";
   nix.settings = {
     substituters = [
@@ -130,12 +131,8 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall.checkReversePath = false;
+  networking.wireguard.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 }
